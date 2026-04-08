610b7bcc9d2ae9fd0c7f4527126f9dfc0c98fa30
maltajohnny <malta.johnny@icloud.com>
2026-04-08 12:17:20 -0300

    chore(cleanup): remove exported board asset folders
    Delete the large exported folders Tina APP Team Stories Board - Boards_files and card_aberto_files from the repository to keep the root clean and reduce repository bloat.

--- Tina APP Team Stories Board - Boards_files/ie.js
+++
@@ -1,2 +0,0 @@
-!function(e){"use strict";"function"==typeof define&&define.amd?define("jQueryUI/ie",["jquery","./version"],e):e(jQuery)}(function(e){"use strict";return e.ui.ie=!!/msie [\w.]+/.exec(navigator.userAgent.toLowerCase())});
-
--- Tina APP Team Stories Board - Boards_files/version(1).js
+++
@@ -1,2 +0,0 @@
-!function(e){"use strict";"function"==typeof define&&define.amd?define("jQueryUI/version",["jquery"],e):e(jQuery)}(function(e){"use strict";e.ui=e.ui||{},e.ui.version="1.13.1"});
-
